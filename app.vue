d44eb18a8e86ce7484e845ade5687ebec12ad281
SuperKojima <hayato.kojima@steamship.co.jp>
2025-01-14 13:24:07 +0900

    pagesの.vueをコピペ

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
+<script setup lang="ts">
+const { data, status, refresh } = await useFetch('/api');
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- refresh -->
+    <button @click="() => refresh()">
+      refresh
+    </button>
+
+    <!-- status, data -->
+    <table border="1">
+      <tr>
+        <td>status</td>
+        <td>{{ status }}</td>
+      </tr>
+      <tr>
+        <td>data</td>
+        <td><pre>{{ data }}</pre></td>
+      </tr>
+    </table>
   </div>
 </template>
